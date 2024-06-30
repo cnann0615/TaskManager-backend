cc0602f7f4e2a2637f577dc293d13876b9c11623
cnann0615 <leave.it.to.7010@gmail.com>
2024-06-30 22:36:55 +0900

    0630　フロントエンドのUIを整えた際の微調整＆細かなバグ解消

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 -- src/main/resources/data.sql
-INSERT INTO task_category (name, order_index) VALUES ('なし', 1);
-INSERT INTO task_schedule (name, order_index) VALUEs ('なし', 1);
+INSERT INTO task_category (name, order_index) VALUES ('None', 1);
+INSERT INTO task_schedule (name, order_index) VALUEs ('None', 1);
