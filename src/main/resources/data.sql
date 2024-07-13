bc8d4991c6d01a93d4a0308025266ad147bab5be
cnann0615 <leave.it.to.7010@gmail.com>
2024-07-13 09:24:43 +0900

    0713 data.sqlを削除

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
--- src/main/resources/data.sql
-INSERT INTO task_category (user_id, name, order_index) VALUES ('dummy', 'None', 1);
-INSERT INTO task_schedule (user_id, name, order_index) VALUES ('dummy', 'None', 1);
